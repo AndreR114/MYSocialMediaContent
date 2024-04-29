e4f4c4b2c78e0dd01e41e5aac9e999c59914fb47
AndreR114 <129870218+AndreR114@users.noreply.github.com>
2024-04-29 12:53:32 +0200

    Add files via upload

--- MSMC.js
+++ MSMC.js
@@ -1,42 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var video = document.getElementById("background-video");
-  if (video) {
-    video.playbackRate = 0.5; // Stellt die Abspielgeschwindigkeit auf 60% der normalen Geschwindigkeit
+function toggleResetButton() {
+  const expanded = document.querySelector(".expanded");
+  const resetButton = document.getElementById("reset-button");
+  if (expanded) {
+    resetButton.style.display = "block"; // Zeigt den Button an, wenn ein Bereich expandiert ist
+  } else {
+    resetButton.style.display = "none"; // Versteckt den Button, wenn kein Bereich expandiert ist
   }
-});
+}
 
-let currentSlide = 0;
-const images = document.querySelectorAll(".carousel-images img");
-const totalImages = images.length;
-
-function showSlide(index) {
-  const angle = 360 / totalImages;
-  images.forEach((img, i) => {
-    img.style.transition = "transform 1.5s ease-in-out"; // Stellt sicher, dass auch hier eine sanfte Transition angewendet wird
-    img.style.transform = `rotateY(${
-      (i - index) * angle
-    }deg) translateZ(400px)`; // Erhöhe den Wert für einen stärkeren 3D-Effekt
-    img.style.opacity = i === index ? 1 : 0.6;
+document
+  .getElementById("tiktok-section")
+  .addEventListener("click", function () {
+    if (!this.classList.contains("expanded")) {
+      this.classList.add("expanded");
+      document.getElementById("instagram-section").classList.remove("expanded");
+    }
+    toggleResetButton();
   });
-}
 
-function moveSlide(step) {
-  currentSlide = (currentSlide + step + totalImages) % totalImages;
-  showSlide(currentSlide);
-}
-
-showSlide(currentSlide); // Initialer Aufruf, um das erste Bild
-function openOverlay(src) {
-  document.getElementById("overlayImage").src = src;
-  document.getElementById("overlay").style.display = "flex";
-}
-
-function closeOverlay() {
-  document.getElementById("overlay").style.display = "none";
-}
-
-document.querySelectorAll(".carousel-images img").forEach((img) => {
-  img.addEventListener("click", function () {
-    openOverlay(this.src);
+document
+  .getElementById("instagram-section")
+  .addEventListener("click", function () {
+    if (!this.classList.contains("expanded")) {
+      this.classList.add("expanded");
+      document.getElementById("tiktok-section").classList.remove("expanded");
+    }
+    toggleResetButton();
   });
+
+document.getElementById("reset-button").addEventListener("click", function () {
+  document.getElementById("tiktok-section").classList.remove("expanded");
+  document.getElementById("instagram-section").classList.remove("expanded");
+  toggleResetButton();
 });
