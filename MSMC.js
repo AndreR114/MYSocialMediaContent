40a1e1fb10bacc2b9c6874c1c1cd3db3701ffd6d
AndreR114 <129870218+AndreR114@users.noreply.github.com>
2024-04-30 07:34:55 +0200

    Add files via upload

--- MSMC.js
+++ MSMC.js
@@ -76,3 +76,46 @@ function updateGalleryTransform() {
     nextButton.classList.remove("disabled");
   }
 }
+/*===========================================INSTASLIDER===========================================*/
+function updateGalleryTransform2() {
+  const slideWidth2 = slides2[0].clientWidth; // Stellt sicher, dass die Breite korrekt abgerufen wird
+  const offset2 = slideIndex2 * slideWidth2;
+  const slider2 = document.querySelector(".gallery-slider-insta");
+  slider2.style.transform = `translateX(-${offset2}px)`;
+
+  const prevButton2 = document.querySelector("prev-insta");
+  const nextButton2 = document.querySelector("next-insta");
+
+  // Setzt die 'disabled' Klasse korrekt
+  if (slideIndex2 === 0) {
+    prevButton2.classList.add("disabled");
+  } else {
+    prevButton2.classList.remove("disabled");
+  }
+
+  if (slideIndex2 === totalSlides2 - 1) {
+    nextButton2.classList.add("disabled");
+  } else {
+    nextButton2.classList.remove("disabled");
+  }
+}
+
+let slideIndex2 = 0; // Startindex des Sliders
+const slides2 = document.querySelectorAll(".gallery-slider-insta img"); // Alle Slides
+const totalSlides2 = slides2.length; // Gesamtzahl der Slides
+
+document.querySelector(".next-insta").addEventListener("click", () => {
+  if (slideIndex2 < totalSlides2 - 1) {
+    // Überprüft, ob das aktuelle Bild nicht das letzte ist
+    slideIndex2++; // Erhöht den Index nur, wenn es nicht das letzte Bild ist
+    updateGalleryTransform2();
+  }
+});
+
+document.querySelector(".prev-insta").addEventListener("click", () => {
+  if (slideIndex2 > 0) {
+    // Stellt sicher, dass der Index nicht negativ wird
+    slideIndex2--; // Verringert den Index nur, wenn es nicht das erste Bild ist
+    updateGalleryTransform2();
+  }
+});
